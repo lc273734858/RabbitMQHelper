eddb38396e23334b85b94c46b3f0e587f130e9e1
刘吉国 <liujg@5173.com>
2018-03-08 09:44:31 +0800

    增加Routing参数

--- EventBus2RMQ/Producter.cs
+++ EventBus2RMQ/Producter.cs
@@ -44,12 +44,23 @@ namespace EventBus2RMQ
         /// 
         /// </summary>
         /// <param name="data"></param>
-        /// <param name="eventName"></param>
+        /// <param name="eventName">事件名称</param>
         /// <param name="Persistent">数据是否持久化，默认持久化</param>
         static public void PushData(object data, string eventName, bool Persistent = true)
         {
+            PushDataByRoutingKey(data, eventName,"", Persistent);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="eventName">事件名称</param>
+        /// <param name="routing">路由</param>
+        /// <param name="Persistent">是否持久化</param>
+        static public void PushDataByRoutingKey(object data, string eventName, string routing, bool Persistent = true)
+        {
             var jsondata = JsonConvert.SerializeObject(data, jsonset);
-            PushData(jsondata, eventName, Persistent);
+            PushDataByRoutingKey(jsondata, eventName,routing, Persistent);
         }
         /// <summary>
         /// 
@@ -59,8 +70,19 @@ namespace EventBus2RMQ
         /// <param name="Persistent"></param>
         static public void PushData(string jsondata, string eventName, bool Persistent = true)
         {
+            PushDataByRoutingKey(jsondata, eventName,"", Persistent);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jsondata"></param>
+        /// <param name="eventName">事件名称</param>
+        /// <param name="routing">路由</param>
+        /// <param name="Persistent"></param>
+        static public void PushDataByRoutingKey(string jsondata, string eventName, string routing, bool Persistent = true)
+        {
             var body = Encoding.UTF8.GetBytes(jsondata);
-            PushData(body, eventName, Persistent);
+            PushDataByRoutingKey(body, eventName, routing, Persistent);
         }
         /// <summary>
         /// 
@@ -70,6 +92,17 @@ namespace EventBus2RMQ
         /// <param name="Persistent"></param>
         static public void PushData(byte[] body, string eventName, bool Persistent = true)
         {
+            PushDataByRoutingKey(body, eventName, "", Persistent);
+        }
+        /// <summary>
+        /// 根据路由发送事件
+        /// </summary>
+        /// <param name="body"></param>
+        /// <param name="eventName"></param>
+        /// <param name="routing"></param>
+        /// <param name="Persistent"></param>
+        static public void PushDataByRoutingKey(byte[] body, string eventName,string routing, bool Persistent = true)
+        {
             using (IModel channel = Connection.CreateModel())
             {
                 channel.ConfirmSelect();
@@ -77,7 +110,7 @@ namespace EventBus2RMQ
                 var prop = channel.CreateBasicProperties();
                 prop.Persistent = Persistent;
 
-                channel.BasicPublish(eventName, "", prop, body);
+                channel.BasicPublish(eventName, routing, prop, body);
 
                 if (!channel.WaitForConfirms())
                 {
@@ -96,8 +129,12 @@ namespace EventBus2RMQ
         /// <param name="Persistent">数据是否持久化，默认持久化</param>
         static public void PushDataAsync(object data, string eventName, ErrorHandler errorCallBack, bool Persistent = true)
         {
+            PushDataByRoutingAsync(data, eventName,"", errorCallBack, Persistent);
+        }
+        static public void PushDataByRoutingAsync(object data, string eventName,string routing, ErrorHandler errorCallBack, bool Persistent = true)
+        {
             var jsondata = JsonConvert.SerializeObject(data, jsonset);
-            PushDataAsync(jsondata, eventName, errorCallBack, Persistent);
+            PushDataByRoutingAsync(jsondata, eventName, routing, errorCallBack, Persistent);
         }
         /// <summary>
         /// 
@@ -108,15 +145,24 @@ namespace EventBus2RMQ
         /// <param name="Persistent"></param>
         static public void PushDataAsync(string jsondata, string eventName, ErrorHandler errorCallBack, bool Persistent = true)
         {
+            PushDataByRoutingAsync(jsondata, eventName,"", errorCallBack, Persistent);
+        }
+        static public void PushDataByRoutingAsync(string jsondata, string eventName,string routing, ErrorHandler errorCallBack, bool Persistent = true)
+        {
             var body = Encoding.UTF8.GetBytes(jsondata);
-            PushDataAsync(body, eventName, errorCallBack, Persistent);
+            PushDataByRoutingAsync(body, eventName, routing, errorCallBack, Persistent);
         }
         static public void PushDataAsync(byte[] body, string eventName, ErrorHandler errorCallBack, bool Persistent = true)
+        {
+            PushDataByRoutingAsync(body, eventName, "", errorCallBack, Persistent);
+
+        }
+        static public void PushDataByRoutingAsync(byte[] body, string eventName,string routing, ErrorHandler errorCallBack, bool Persistent = true)
         {
             var hander = new PushDataHandler((body1, eventname1, Persistent1) => {
                 try
                 {
-                    PushData(body1, eventname1, Persistent1);
+                    PushDataByRoutingKey(body1, eventname1, routing, Persistent1);
                     Console.WriteLine("成功");
                     throw new Exception("推送失败");
                 }
@@ -155,13 +201,14 @@ namespace EventBus2RMQ
         /// </summary>
         /// <param name="jsondata"></param>
         /// <param name="eventName">事件名称</param>
+        /// <param name="routing"></param>
         /// <param name="times">重试次数</param>
         /// <param name="interval">重试时间间隔/毫秒</param>
         /// <param name="Persistent">数据是否持久化，默认持久化</param>
         static public void PushDataAutoRetry(string jsondata, string eventName, int times, int interval, bool Persistent = true)
         {
             var body = Encoding.UTF8.GetBytes(jsondata);
-            PushDataAutoRetry(body, eventName, times, interval, Persistent);
+            PushDataAutoRetry(body, eventName, "",times, interval, Persistent);
 
         }
         /// <summary>
@@ -169,10 +216,11 @@ namespace EventBus2RMQ
         /// </summary>
         /// <param name="body"></param>
         /// <param name="eventName">事件名称</param>
+        /// <param name="routing"></param>
         /// <param name="times">重试次数</param>
         /// <param name="interval">重试时间间隔/毫秒</param>
         /// <param name="Persistent">数据是否持久化，默认持久化</param>
-        static private void PushDataAutoRetry(byte[] body, string eventName, int times, int interval, bool Persistent = true)
+        static private void PushDataAutoRetry(byte[] body, string eventName,string routing, int times, int interval, bool Persistent = true)
         {
             for (int i = 1; i <= times; i++)
             {
@@ -184,7 +232,7 @@ namespace EventBus2RMQ
                         var prop = channel.CreateBasicProperties();
                         prop.Persistent = Persistent;
 
-                        channel.BasicPublish(eventName, "", prop, body);
+                        channel.BasicPublish(eventName, routing, prop, body);
 
                         if (!channel.WaitForConfirms())
                         {
@@ -214,7 +262,7 @@ namespace EventBus2RMQ
         /// <param name="eventName"></param>
         /// <param name="Persistent"></param>
         /// <param name="ErrorCallBack"></param>
-        static public void PushDatas<T>(ICollection<T> datas, string eventName, bool? Persistent, Action<T> ErrorCallBack)
+        static public void PushDatas<T>(ICollection<T> datas, string eventName,string routing,bool? Persistent, Action<T> ErrorCallBack)
         {
             using (IModel channel = Connection.CreateModel())
             {
@@ -227,7 +275,7 @@ namespace EventBus2RMQ
                         var body = Encoding.UTF8.GetBytes(bodystring);
                         var prop = channel.CreateBasicProperties();
                         prop.Persistent = Persistent.GetValueOrDefault(true);
-                        channel.BasicPublish(eventName, "", prop, body);
+                        channel.BasicPublish(eventName, routing, prop, body);
 
                         if (!channel.WaitForConfirms())
                         {
@@ -240,6 +288,10 @@ namespace EventBus2RMQ
                     throw;
                 }
             }
+        }
+        static public void PushDatas<T>(ICollection<T> datas, string eventName, bool? Persistent, Action<T> ErrorCallBack)
+        {
+            PushDatas(datas, eventName, "", Persistent, ErrorCallBack);
         }
         #endregion
     }
